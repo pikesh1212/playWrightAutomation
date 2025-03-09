ef701e6374da2f354858f486d7d264abdf5c462e
Pikesh Patel <pvpatel@AD.MSYSTECHNOLOGIES.COM>
2025-03-09 14:21:57 +0530

    inital test cases

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
+  timeout: 40*1000,
+  expect:{
+    timeout: 5000,
+  },
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -31,6 +35,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
@@ -40,15 +45,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+test("page playwright test", async({page})=>{
+  await page.goto("https://google.com")
+  console.log("page title is - ",await page.title())
+  await expect(page).toHaveTitle("Google")
+  // css and xpath
+})
+
+test.only("input field test",async ({page})=>{
+  await page.goto("https://rahulshettyacademy.com/angularpractice/")
+  await page.locator("[minlength='2']").fill("rahul")
+})
